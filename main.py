7bb90d241b0cf2d3b10b63b86b700a955ea78126
Subhajit Sahu <wolfram77@gmail.com>
2019-03-29 14:24:48 +0530

    save

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
-from sanic import Sanic
-from sanic.response import json
 
-app = Sanic()
 
-@app.route('/')
-async def test(request):
-    return json({'hello': 'world'})
+def parse_addr(addr):
+  i = addr.find(':')
+  host = '' if i<0 else addr[0:i]
+  port = int(addr if i<0 else addr[i+1:])
+  return (host, port)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+
+class RequestHandler(BaseHTTPRequestHandler):
+  def body(self):
+    size = int(self.headers.get('Content-Length'))
+    return self.rfile.read(size)
+
+  def send(self, code, body=None, headers=None):
+    self.send_response(code)
+    for k, v in headers.items():
+      self.send_header(k, v)
+    self.end_headers()
+    if body is not None:
+      self.wfile.write(body)
+  
+  def send_json(self, code, body):
+    heads = {'Content-Type': 'application/json'}
+    self.send(code, bytes(json.dumps(body), 'utf8'), heads)
+
+  def do_GET(self):
+    handler = self.server.handler
+    self.send_json(200, handler.addrs)
+
+  def do_POST(self):
+    handler = self.server.handler
+    if self.path.startswith('/service'):
+      return handler.handle_service(self)
+    return handler.handle_forward(self)
